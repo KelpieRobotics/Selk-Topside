f486dc226987a0e5406cf20a4babecfe10456f47
ymoumine <ymoum044@uottawa.ca>
2024-10-25 20:57:57 -0400

    update client

--- tcp-client-program/client.cpp
+++ tcp-client-program/client.cpp
@@ -6,55 +6,55 @@
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h> // read(), write(), close()
-#define MAX 80
+#define MAX 1024
 #define PORT 8080
 #define SA struct sockaddr
+char buff[MAX];
 
 
-void main()
+void func(int connfd) {
+    
+    // read the message from companion server and copy it in buffer
+    read(connfd, buff, sizeof(buff));
+
+    // print buffer which contains the temp data
+    printf("From companion reading: %s \n", buff);
+    
+    close(connfd);
+}
+
+int main()
 {
     int sockfd, connfd;
     struct sockaddr_in servaddr, cli;
 
     // socket create and verification
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd == -1) {
+    if (sockfd < 0) {
         printf("socket creation failed...\n");
-        exit(0);
+        exit(1);
     }
 
     bzero(&servaddr, sizeof(servaddr));
 
     // assign IP, PORT
+
+    struct hostent *server  = gethostbyname("127.0.0.1");
     servaddr.sin_family = AF_INET;
-    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    bcopy((char *)server->h_addr, 
+         (char *)&servaddr.sin_addr.s_addr,
+         server->h_length);
     servaddr.sin_port = htons(PORT);
 
     // connect the client socket to server socket
     if (connect(sockfd, (SA*)&servaddr, sizeof(servaddr))
         != 0) {
         printf("connection with the server failed...\n");
-        exit(0);
+        exit(1);
     }
 
     func(sockfd);
 
-    // initialise sht30 sensor
-    // TwoWire.begin();
-    // Serial.begin(115200);
-    // delay(300);
+    return 0;
 }
 
-void func(int connfd) {
-    char buff[MAX];
-    int n;
-    //for (;;) {
-    
-    // read the message from companion server and copy it in buffer
-    read(connfd, buff, sizeof(buff));
-
-    //printf(buff);
-    // print buffer which contains the temp data
-    printf("From companion reading: %s", buff);
-    //}
-}
