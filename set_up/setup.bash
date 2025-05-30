daa1363dbb1c4f7d2279f381d4a20fc82d48fa60
Hasin Zaman <hasin.zaman1@gmail.com>
2025-05-30 18:28:14 -0400

    update setup.bash

--- set_up/setup.bash
+++ set_up/setup.bash
@@ -1,17 +1,19 @@
 sudo apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev libgstreamer-plugins-bad1.0-dev gstreamer1.0-plugins-base gstreamer1.0-plugins-good gstreamer1.0-plugins-bad gstreamer1.0-plugins-ugly gstreamer1.0-libav gstreamer1.0-tools gstreamer1.0-x gstreamer1.0-alsa gstreamer1.0-gl gstreamer1.0-gtk3 gstreamer1.0-qt5 gstreamer1.0-pulseaudio
+
 sudo apt install -y libgtk-3-dev libopencv-dev libsdl2-dev libspdlog-dev cmake gcc g++ python3 python3-pip
 
-sudo apt install -y meson
-sudo apt install -y ninja-build
-# sudo meson install
+# sudo apt install -y meson
+# sudo apt install -y ninja-build
+# # sudo meson install
 
 
-git clone https://github.com/GStreamer/gst-plugins-bad.git
-cd ./gst-plugins-bad
+# git clone https://github.com/GStreamer/gst-plugins-bad.git
+# cd ./gst-plugins-bad
 
-meson -C build
-sudo ninja -C build install
-cd ..
+# meson -C build
+# cd build
+# sudo ninja install
+# cd ..
 
 sudo apt install -y python3-gi python3-gi-cairo gir1.2-gtk-4.0
 pip install pycairo
