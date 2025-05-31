9cb4938971c7b048f26c348342b234d60ebe655d
Hasin Zaman <hasin.zaman1@gmail.com>
2025-05-30 20:02:26 -0400

    update setup

--- set_up/calibrate.py
+++ set_up/calibrate.py
@@ -13,13 +13,7 @@ Gst.init(None)
 
 def create_pipeline(port: int, width: int, height: int, square_size: float) -> str:
     return f"""
-udpsrc port={port} caps="application/x-rtp, media=video, encoding-name=H264, payload=96" !
-rtph264depay ! h264parse ! avdec_h264 ! videoconvert !
-video/x-raw,format=RGB,width=640,height=480 !
-cameracalibrate pattern=chessboard board-width={width} board-height={height} square-size={square_size} show-corners=true frame-count=25 name=calib !
-tee name=t
-t. ! queue ! autovideosink
-t. ! queue ! fakesink
+udpsrc port={port} caps="application/x-rtp, media=video, encoding-name=H264, payload=96" ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! queue ! cameracalibrate pattern=chessboard board-width={width} board-height={height} square-size={square_size} show-corners=true frame-count=25 ! videoconvert ! autovideosink
 """
 
 def calibrate_camera(pipeline_desc: str, file_name: str):
--- set_up/setup.bash
+++ set_up/setup.bash
@@ -10,8 +10,12 @@ pip install PyGObject
 # calibrate cameras need to be on
 
 
-gst-launch-1.0 -v udpsrc port=5601 caps="application/x-rtp, media=video, encoding-name=H264, payload=96" ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! queue ! cameracalibrate pattern=chessboard board-width=6 board-height=8 square-size=7 show-corners=true frame-count=25 ! output=cam_1.xml ! videoconvert ! autovideosink
+# gst-launch-1.0 -v udpsrc port=5600 caps="application/x-rtp, media=video, encoding-name=H264, payload=96" !
+# rtph264depay ! h264parse ! avdec_h264 ! cameracalibrate pattern=chessboard board-width=6 board-height=8 square-size=7.5 show-corners=true frame-count=25 ! output=camera_calib.xml ! fakesink
 
-gst-launch-1.0 -v udpsrc port=5602 caps="application/x-rtp, media=video, encoding-name=H264, payload=96" ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! queue ! cameracalibrate pattern=chessboard board-width=6 board-height=8 square-size=7 show-corners=true frame-count=25 ! output=cam_1.xml ! videoconvert ! autovideosink
+# output=camera_calib.xml ! fakesink
+python3 ./set_up/calibrate.py 5601 cam_1 --width 7 --height 9 --size 7
 
-gst-launch-1.0 -v udpsrc port=5603 caps="application/x-rtp, media=video, encoding-name=H264, payload=96" ! rtph264depay ! h264parse ! avdec_h264 ! videoconvert ! queue ! cameracalibrate pattern=chessboard board-width=6 board-height=8 square-size=7 show-corners=true frame-count=25 ! output=cam_1.xml ! videoconvert ! autovideosink
+python3 ./set_up/calibrate.py 5602 cam_2 --width 7 --height 9 --size 7
+
+python3 ./set_up/calibrate.py 5603 cam_3 --width 7 --height 9 --size 7
