11723a181d2314f51b03bf00db967d9516a0aaa4
Fufs <42976807+Fufs@users.noreply.github.com>
2025-06-02 16:23:20 -0400

    fix typo in network-sharing.sh script

--- scripts/network-sharing.sh
+++ scripts/network-sharing.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
-ETHERNET_INTERFACE=enp42s0
-WIFI_INTERFACE=wlo1
+ETHERNET_INTERFACE="enp42s0"
+WIFI_INTERFACE="wlo1"
 
 sudo systemctl start isc-dhcp-server
 sudo sysctl -w net.ipv4.ip_forward=1
