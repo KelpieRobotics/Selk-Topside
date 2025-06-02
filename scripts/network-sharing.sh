fb0f9d5f681a072a0e48ca3d6abbd6977556eda7
Fufs <42976807+Fufs@users.noreply.github.com>
2025-06-02 16:25:47 -0400

    fix network-sharing.sh script fr this time

--- scripts/network-sharing.sh
+++ scripts/network-sharing.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/bash
 
-ETHERNET_INTERFACE="enp42s0"
-WIFI_INTERFACE="wlo1"
+ETH_IF="enp42s0"
+WLO_IF="wlo1"
 
 sudo systemctl start isc-dhcp-server
 sudo sysctl -w net.ipv4.ip_forward=1
-sudo iptables -A FORWARD -i ${ETHERNET} -j ACCEPT
-sudo iptables -t nat -A POSTROUTING -o ${WIFI} -j MASQUERADE
+sudo iptables -A FORWARD -i ${ETH_IF} -j ACCEPT
+sudo iptables -t nat -A POSTROUTING -o ${WLO_IF} -j MASQUERADE
 
